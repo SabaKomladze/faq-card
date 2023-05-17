2290ad406dc09e07b2d792b417598ca3837dd11f
Saba Komladze <121091520+SabaKomladze@users.noreply.github.com>
2023-05-17 19:37:44 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import { question } from "./Questions";
 import mobile from "./assets/illustration-woman-online-mobile.svg";
 import desktop from "./assets/illustration-woman-online-desktop.svg";
 import SingleQuestion from "./SingleQuestion";
+import box from "./assets/illustration-box-desktop.svg";
+import mobpattern from "./assets/bg-pattern-mobile.svg";
+import deskpattern from "./assets/bg-pattern-desktop.svg";
 function App() {
   const [quests] = useState(question);
   return (
     <div className="app">
+      <div className="layer">
+        <img src={deskpattern} alt="" className="desk-pattern" />
+        <img src={mobpattern} alt="" className="mob-pattern" />
+      </div>
+
       <img src={mobile} alt="" className="mobile" />
       <img src={desktop} alt="" className="desktop" />
-      <article>
+      <img src={box} alt="" className="box" />
+
+      <article className="faq">
         <h1 className="text">FAQ</h1>
         {quests.map((quest, index) => (
           <SingleQuestion
